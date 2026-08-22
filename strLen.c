bd50a65d9c4097d710d3b228c96df33fb1bab398
Debanjan Das <microsoft2420@gmail.com>
2026-08-23 01:00:16 +0530

    Add str_length function to calculate string length and main function for testing

---
+++ strLen.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int str_length(char *str){
+    size_t length = 0 ;
+    while(str[length] != '\0'){
+        length++ ;
+    }
+    if(length == 0){
+        return 0;
+    }
+    return length;
+}
+
+int main(){
+    char str[] = "H";
+    int len = str_length(str);
+    printf("Length is: %d",len);
+    return 0;
+}
