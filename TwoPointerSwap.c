f409143f8db17602213208d8cd61d2508a660589
Debanjan Das <microsoft2420@gmail.com>
2026-08-23 00:05:54 +0530

    Fix swap function to correctly swap values and add output for swapped numbers

--- TwoPointerSwap.c
+++ TwoPointerSwap.c
@@ -2,10 +2,9 @@
 #include<stdlib.h>
 
 void swap(int *a , int *b){
-    int temp = a ;
-    a = b ;
-    b = temp ;
-    printf("After: %d %d",a,b);
+    int temp = *a ;
+    *a = *b ;
+    *b = temp ;
 }
 
 int main(int argc , char *argv[]){
@@ -16,6 +15,8 @@ int main(int argc , char *argv[]){
     int num1 , num2 = 0;
     num1 = atoi(argv[1]);
     num2 = atoi(argv[2]);
+    printf("num1 %d num2 %d\n",num1,num2);
     swap(&num1 , &num2);
+    printf("num1 %d num2 %d",num1,num2);
     return 0;
 }
