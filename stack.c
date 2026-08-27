c61333e7bf6c48e711bc0f5d17d5cda64491859e
Debanjan Das <microsoft2420@gmail.com>
2026-08-27 12:03:20 +0530

    Add stack implementation with push, pop, and read functions

---
+++ stack.c
@@ -0,0 +1,50 @@
+﻿#include<stdio.h>
+#define SIZE 5
+int stack[SIZE] = {0};
+int top = -1;
+
+void push(int num){
+    if(top == SIZE - 1){
+        perror("Error: Stack overflow\n");
+        return;
+    }
+    stack[++top] = num;
+    printf("%d is pushed\n",num);
+    return;
+}
+
+void pop(){
+    if(top == -1){
+        perror("Error: Stack underflow\n");
+        return;
+    }
+    top--;
+    printf("Poped\n");
+    return;
+}
+
+void read(){
+    if(top == -1){
+        printf("Nothing to return\n");
+        return;
+    }
+    int i = 0;
+    while(i <= top){
+        printf("%d\n",stack[i]);
+        i++;
+    }
+}
+
+int main(){
+    printf("********************Stack********************\n");
+    for(int i = 0 ; i < 5 ; i++){
+        int temp;
+        printf("Enter %dth position : ",i);
+        scanf("%d",&temp);
+        push(temp);
+    }
+    pop();
+    read();
+    printf("Comeplete");
+    return 0;
+}
